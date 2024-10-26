de27f83abf304b7a955740a418912fbcfab139c7
Ekky Patria Kencana <ekkypatria@gmail.com>
2024-10-27 01:26:29 +0700

    [restructured]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './assets/**/*.{html,js}',
+    './src/**/*.{html,js}',
+    './index.html',
+  ],
   theme: {
     extend: {
       colors: {
